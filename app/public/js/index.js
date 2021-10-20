11db90874b048aed80f9d81632c2d78cb87f6c50
mohanprabhu10 <mprabhu22012@gmail.com>
2021-10-20 14:08:13 -0400

    Create API for book form

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -10,7 +10,11 @@ const app = { //const is faster, is not going to change
             "country":"USA",
             "birthday":"",
         },
-        "imgURL":"img/Batman.jpg" //default image URL
+        "imgURL":"img/Batman.jpg", //default image URL
+        offerForm:{},
+        selectedOffer:{},
+        books:{},
+        bookForm:{}
     }
     },
 
@@ -37,19 +41,81 @@ const app = { //const is faster, is not going to change
                 this.randomPerson.country=fetchedUserDetails.location.country;
                 this.imgURL=fetchedUserDetails.picture.large;
                 console.log(this.randomPerson);
-                console.log(this.randomPerson.image_URL_medium);
+                //console.log(this.randomPerson.image_URL_medium);
             }) //anon function, can use multiple statements
 
             .catch((err) => {
                 console.error(err);
             })
 
+        },
+        fetchBookData()
+        {
+            fetch('/api/book/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.books = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        postNewOffer(evt)
+        {
+            console.log("Posted!");
+            //alert("created");
+            fetch('api/book/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then(response => response.json() )
+            .then(json => {
+                console.log("Returned from post");
+                this.fetchBookData();
+            });
+            
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
         }
+        // selectOfferToEdit(o)
+        // {
+        //     this.selectedOffer = o;
+        //     this.offerForm = this.selectedOffer;
+        // }
+        // resetForm()
+        // {
+        //     this.selectedOffer = null;
+        //     this.offerForm = {};
+        // },
+        // postEditOffer(evt)
+        // {
+        //     this.offerForm.studentId = this.selectdd
+        //     console.log("Updated!");
+        //     //alert("created");
+        //     fetch('api/book/update.php', {
+        //         method:'POST',
+        //         body: JSON.stringify(this.bookForm),
+        //         headers: {
+        //             "Content-Type": "application/json; charset=utf-8"
+        //         }
+        //     })
+        //     .then(response => response.json() )
+        //     .then(json => {
+        //         comsole.log("Returned from post");
+        //     })
+        // },
     },
 
     created() //event hook
     {
         this.fetchUserData();
+        this.fetchBookData();
     }
   }
   console.log("Creating and Mounting Vue Application");
